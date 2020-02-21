c0f33279b9df97555d5f3afd7775030b7d27ab3c
chawki <gasmi@orevonlabs.com>
2020-02-21 15:50:07 +0100

    update header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   constructor(
-    public authentificationService: AuthentificationService,
+    private authentificationService: AuthentificationService,
     private router: Router
   ) { }
   visibility = false;
